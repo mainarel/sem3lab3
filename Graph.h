09eddef287c84f7c2009fc862aaddecce345f150
mainarel <64072803+mainarel@users.noreply.github.com>
2021-01-26 01:53:07 +0300

    Add files via upload

--- Graph.h
+++ Graph.h
@@ -82,7 +82,6 @@ public:
 	}
 
 	int whereArc(ID first, ID second) {
-		int size = getArcsCount();
 		for (int i = 0; i < this->getArcsCount(); ++i) {
 			if (this->arcs->Get(i)->getArc().first->getID() == first && this->arcs->Get(i)->getArc().second->getID() == second) {
 				return i;
@@ -91,6 +90,14 @@ public:
 		return -1;
 	}
 
+	int whereVertex(ID id) {
+		for (int i = 0; i < this->getVerticesCount(); ++i) {
+			if (id == this->vertices->Get(i)->getID()) {
+				return i;
+			}
+		}
+		return -1;
+	}
 
 	void addVertex(ID id) {
 		Vertex<ID>* new_vertex = new Vertex<ID>(id);
@@ -131,6 +138,23 @@ public:
 		}
 	}
 
+	void removeVertex(ID id) {
+		if (HasVertex(id)) {
+			for (int i = 0; i < this->getArcsCount(); ++i) {
+				if (this->arcs->Get(i)->getArc().first->getID() == id) {
+					removeArc(this->arcs->Get(i)->getArc().first->getID(), this->arcs->Get(i)->getArc().second->getID());
+				}
+				if (this->arcs->Get(i)->getArc().second->getID() == id) {
+					removeArc(this->arcs->Get(i)->getArc().second->getID(), this->arcs->Get(i)->getArc().first->getID());
+				}
+			}
+			this->vertices->RemoveAt(this->whereVertex(id));
+		}
+		else {
+			cout << "This vertex was not found in the graph"<<endl;
+		}
+	}
+
 	// Adjacency Matrix
 	vector<vector<int>> getAdjMatrix() {
 		vector<vector<int>> matrix(this->getVerticesCount(), vector<int>(this->getVerticesCount(), 0));
@@ -144,22 +168,121 @@ public:
 				matrix[i][j] = 9999;
 			}
 		}
-
 		for (int i = 0; i < this->getArcsCount(); ++i) {
 			int frst = 0;
 			int scnd = 0;
 			for (int j = 0; j < this->getVerticesCount(); ++j) {
-				if (this->arcs->Get(i)->getArc().first->getID() == vertices->Get(j)->getID()) { frst = j; }
-				if (this->arcs->Get(i)->getArc().second->getID() == vertices->Get(j)->getID()) { scnd = j; }
+				if (this->arcs->Get(i)->getArc().first->getID() == vertices->Get(j)->getID()) { 
+					frst = j; 
+				}
+				if (this->arcs->Get(i)->getArc().second->getID() == vertices->Get(j)->getID()) {
+					scnd = j; 
+				}
 			}
 			if (matrix[frst][scnd] > this->arcs->Get(i)->getWeight()) {
 				matrix[frst][scnd] = this->arcs->Get(i)->getWeight();
 			}
 		}
-
-		
-
 		return matrix;
 	}
 
+	void dijkstra(string StartVertex, string EndVertex) {
+		int INF = 9999;
+		int startnode;
+		int endnode;
+		for (int i = 0; i < this->getVerticesCount(); i++) {
+			if (this->vertices->Get(i)->getID() == StartVertex) {
+				startnode = i;
+			}
+			if (this->vertices->Get(i)->getID() == EndVertex) {
+				endnode = i;
+			}
+		}
+		vector<vector<int>> G = this->getAdjMatrix();
+		vector<vector<int> > cost(this->getVerticesCount(), vector<int>(this->getVerticesCount(), 0));
+		vector<int> distance(this->getVerticesCount()), pred(this->getVerticesCount());
+		vector<int> visited(this->getVerticesCount());
+		int count, mindistance, nextnode, i, j;
+		for (i = 0; i < this->getVerticesCount(); i++)
+			for (j = 0; j < this->getVerticesCount(); j++)
+				if (G[i][j] == 0 || G[i][j] == INF)
+					cost[i][j] = INF;
+				else
+					cost[i][j] = G[i][j];
+		for (i = 0; i < this->getVerticesCount(); i++) {
+			distance[i] = cost[startnode][i];
+			pred[i] = startnode;
+			visited[i] = 0;
+		}
+		distance[startnode] = 0;
+		visited[startnode] = 1;
+		count = 1;
+		while (count < this->getVerticesCount() - 1) {
+			mindistance = INF;
+			for (i = 0; i < this->getVerticesCount(); i++)
+				if (distance[i] < mindistance && !visited[i]) {
+					mindistance = distance[i];
+					nextnode = i;
+				}
+			visited[nextnode] = 1;
+			for (i = 0; i < this->getVerticesCount(); i++)
+				if (!visited[i])
+					if (mindistance + cost[nextnode][i] < distance[i]) {
+						distance[i] = mindistance + cost[nextnode][i];
+						pred[i] = nextnode;
+					}
+			count++;
+		}
+		
+		for (i = 0; i < this->getVerticesCount(); i++)
+			if (i != startnode && i == endnode) {
+				string graphAPI_V;
+				if (distance[i] != INF) {
+					cout << "\nDistance to Vertex <" << this->vertices->Get(i)->getID() << "> = " << distance[i];
+					cout << "\nPath:" << this->vertices->Get(i)->getID();
+					j = i;
+					do {
+						j = pred[j];
+						cout << " <- " << this->vertices->Get(j)->getID();
+					} while (j != startnode);
+					
+				}
+				else {
+					cout << "\n\tYou can't get to Vertex " << this->vertices->Get(endnode)->getID() << " from Vertex '" <<
+						this->vertices->Get(startnode)->getID() << "'\n";
+				}
+			}
+	}
+
+	ListSequence<Vertex<ID>*>* getVetex() {
+		return this->vertices;
+	}
+
+	void PrintAdjMatrix() {
+		cout << "Adjacency Matrix:" << endl;
+		vector<vector<int>> vect = getAdjMatrix();
+		ListSequence<Graph<string>::Vertex<string>*>* Names = getVetex();
+
+		cout << "      ";
+		for (int i = 0; i < getVerticesCount(); i++) {
+			cout << Names->Get(i)->getID() << "    ";
+		}
+		cout << endl;
+		for (int i = 0; i < getVerticesCount(); i++) {
+			cout << Names->Get(i)->getID() << " | ";
+			for (int j = 0; j < getVerticesCount(); j++) {
+
+				cout.width(3);
+				//cout.setf(ios::left);
+
+				if (vect[i][j] != 9999) {
+					cout << vect[i][j] << "  ";
+				}
+				else {
+					cout << "-" << "  ";
+				}
+			}
+			cout << endl;
+		}
+	}
 };
--- Source.cpp
+++ Source.cpp
@@ -1,17 +1,62 @@
 #include <iostream>
 #include "Graph.h"
 #include <string>
-//using namespace std;
+using namespace std;
 
-int main()
-{
-	//cout <<"mew";
+int main() {
 	Graph<string>* graph = new Graph<string>();
-	graph->addVertex("first");
-	graph->addVertex("second");
+	string ID, fID, sID;
+	int weight;
+	while (true) {
+		cout << "\n1. Add vertex\n2. Add arc\n3. Remove vertex (not work)\n4. Remove arc\n5. Print adjacency matrix\n6.Dijkstra algorithm \n7. Exit" << endl;
+		int choice; cin >> choice;
+		switch (choice) {
+		case 1: {
+			cout << "Enter vertex ID" << endl;
+			cin >> ID;
+			graph->addVertex(ID);
+			break;
+		}
+		case 2: {
+			cout << "Enter first vertex ID" << endl;
+			cin >> fID;
+			cout << "Enter second vertex ID" << endl;
+			cin >> sID;
+			cout << "Enter weight of arc" << endl;
+			cin >> weight;
+			graph->addArc(fID, sID, weight);
+			break;
+		}
+		case 3: {
+			cout << "Enter vertex ID to remove" << endl;
+			cin >> ID;
+			graph->removeVertex(ID);
+			break;
+		}
+		case 4: {
+			cout << "Enter first vertex ID of arc to remove" << endl;
+			cin >> fID;
+			cout << "Enter second vertex ID of arc to remove" << endl;
+			cin >> sID;
+			graph->removeArc(fID, sID);
+			break;
+		}
+		case 5: {
+			graph->PrintAdjMatrix();
+			break;
+		}
+		case 6: {
+			cout << "Enter first vertex ID" << endl;
+			cin >> fID;
+			cout << "Enter second vertex ID" << endl;
+			cin >> sID;
+			graph->dijkstra(fID,sID);
+			break;
+		}
+		case 7: {
+			exit(7);
+		}
+		}
+	}
 	
-	graph->addArc("A","B",1);
-	graph->addArc("first", "second", 1);
-	graph->addArc("first", "second", 2);
-//	graph->removeArc("A","B");
 }
